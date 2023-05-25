09b70db3805554bfea1352045d7f1bea81bc7d61
avetisminasyan <avominasyan71@gmail.com>
2023-05-25 08:27:50 +0400

    param adder

--- param_adder/add.v
+++ param_adder/add.v
@@ -1,19 +1,14 @@
-module FullAdder #(
-  parameter DATA_WIDTH = 4
-) (
-  input [DATA_WIDTH-1:0] a,
-  input [DATA_WIDTH-1:0] b,
-  input cin,
-  output [DATA_WIDTH-1:0] sum,
-  output cout
-);
-  
+module FullAdder #(parameter DATA_WIDTH = 4)(a,b,cin,sum,cout);
+  input [data_width-1:0] a,b;
+  input cin;
+  output [data_width-1:0] sum;
+  output cout;
   wire [DATA_WIDTH-1:0] carry;
-  
   generate
     genvar i;
-    for (i = 0; i < DATA_WIDTH; i = i + 1) begin : ADDER_GEN
-      FullAdder fa(
+    for (i = 0; i < DATA_WIDTH; i = i + 1)
+    begin
+      FullAdders fa(
         .a(a[i]),
         .b(b[i]),
         .cin(i == 0 ? cin : carry[i-1]),
@@ -22,20 +17,11 @@ module FullAdder #(
       );
     end
   endgenerate
-  
   assign cout = carry[DATA_WIDTH-1];
-  
 endmodule
-
-module FullAdder (
-  input a,
-  input b,
-  input cin,
-  output sum,
-  output cout
-);
-  
+module FullAdder(a,b,cin,sum,cout);
+  input a,b,cin;
+  output sum,cout;
   assign sum = a ^ b ^ cin;
   assign cout = (a & b) | (cin & (a ^ b));
-  
 endmodule
